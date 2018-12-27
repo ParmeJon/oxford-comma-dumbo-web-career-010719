59538c6babb8a75eccd6ac866a9d931f8ca1c749
Jonathan Chan <jonchan816@gmail.com>
2018-12-27 08:32:20 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,7 @@
 def oxford_comma(array)
   if array.length == 2 
     return array.join(" and ")
-  elsif array.lenght == 1 
+  elsif array.length == 1 
     return array.join
   elsif array.length >= 3
     array[-2] << " and "
