df3bb844bdb1a72cf3eea42fb65f0a2684c83ca0
Jonathan Chan <jonchan816@gmail.com>
2018-12-27 08:33:08 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,7 @@ def oxford_comma(array)
   elsif array.length == 1 
     return array.join
   elsif array.length >= 3
-    array[-2] << " and "
+    array[-2] << "and "
     return array.join(", ")
   
   end
